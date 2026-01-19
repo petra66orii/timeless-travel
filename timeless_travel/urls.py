6270c58c4473ddd8bd85a941c4146d57ebaf3372
Petra <petra66orii@gmail.com>
2026-01-19 20:21:18 +0000

    Add URL for checklists, #31

--- timeless_travel/urls.py
+++ timeless_travel/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from blog.views import BlogPostViewSet
+from checklists.views import ChecklistViewSet, TaskUpdateView
 
 # Create the router and register viewsets
 router = DefaultRouter()
 router.register(r'posts', BlogPostViewSet, basename='blogpost')
+router.register(r'checklists', ChecklistViewSet, basename='checklist')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,6 +16,7 @@ urlpatterns = [
     
     # The API URLs are now live at /api/
     path('api/', include(router.urls)),
+    path('api/tasks/<int:pk>/', TaskUpdateView.as_view(), name='task-update'),
     
     path('', include('home.urls'), name='home'),
     path('blog/', include("blog.urls")),
